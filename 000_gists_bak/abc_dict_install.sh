4269708e5bb56c1afe86577927233a5be0bff9f7
shshixingang <shshixingang@corp.netease.com>
2020-05-21 14:56:28 +0800

    update format

--- 000_gists_bak/abc_dict_install.sh
+++ 000_gists_bak/abc_dict_install.sh
@@ -6,10 +6,10 @@ rm -rf ~/simple_dict
 git clone https://github.com/geekpanshi/simple_dict.git ~/simple_dict
 echo "."
 echo "2. Alias the abc to the dict exe"
-alias abc='python ~/simple_dict/abc.py'
+echo "   such as \"$ alias abc='python ~/simple_dict/abc.py'\""
 echo "."
 echo "3. Test the dict, using as: abc search_world."
-echo '   such as "abc python`"'
-echo '   such as "abc 编程"'
+echo "   such as \"$ abc python\""
+echo "   such as \"$ abc 编程\""
 echo "."
 echo "4. OK, Enjoy yourself"
