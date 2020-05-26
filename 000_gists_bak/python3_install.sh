2f8d14a70b085dcb9942320f6f8b2a4a13ba92d9
shshixingang <shshixingang@corp.netease.com>
2020-05-26 18:21:57 +0800

    update install configure

--- 000_gists_bak/python3_install.sh
+++ 000_gists_bak/python3_install.sh
@@ -2,8 +2,7 @@
 
 wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
 tar -zxvf Python-3.7.0.tgz && cd Python-3.7.0
-./configure prefix=/usr/local/python3 && make && make install
-ln -s /usr/local/python3/bin/python3.7 /usr/bin/python3
+./configure prefix=/usr/local/python3 --with-ssl && make && make install
 ln -s /usr/local/python3/bin/python3.7 /usr/bin/python3
 python3 -V
 pip3 --version
