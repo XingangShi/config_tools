681ecd6d9b3d47e73c5a3512db3dfa5223f37ec2
shixingang <shshixingang@corp.netease.com>
2020-06-12 19:15:57 +0800

    open

--- 000_gists_bak/init_oh-my-zsh.sh
+++ 000_gists_bak/init_oh-my-zsh.sh
@@ -33,4 +33,7 @@ autojump --version
 #plugins=(git
 #         zsh-autosuggestions
 #         autojump
+#         zsh-syntax-highlighting
 #)
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
